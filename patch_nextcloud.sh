2b5f3fe01002ba4604040b4e6ed425cd090a8553
Kay Probst <mail@kay.sh>
2020-03-21 21:23:15 +0100

    Init

---
+++ patch_nextcloud.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose exec --user www-data app php occ config:system:set overwriteprotocol --value="https"
+
+docker-compose exec --user www-data app php occ db:add-missing-indices
+
+docker-compose exec --user www-data app php occ db:convert-filecache-bigint
+
+echo "Done!"
